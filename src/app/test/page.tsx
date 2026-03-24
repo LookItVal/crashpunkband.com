46254ced7433e236cbacb23bddd0099f923685f6
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2026-03-24 16:04:57 -0500

    Copmpressed audio files

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -10,7 +10,7 @@ import AudioPlayer from "@/components/CRASHTheme/AudioPlayer/AudioPlayer";
 export default function TestPage() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <AudioPlayer audioSrc="/audio/Cut Loose.wav" />
+      <AudioPlayer audioSrc="/audio/Cut Loose.mp3" />
     </main>
   );
 }
